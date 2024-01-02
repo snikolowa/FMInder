ef467226a239191e25ab17fa05b72c240ce1ddf6
snikolowa <sisito_nikolova@abv.bg>
2024-01-02 18:30:29 +0200

    Initial commit

---
+++ src/main/java/com/example/fminder/FminderApplication.java
@@ -0,0 +1,13 @@
+package com.example.fminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fminder/FminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
