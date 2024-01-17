b4be197b9f8dff8b0239456e39339fa78cd9d263
snikolowa <sisito_nikolova@abv.bg>
2024-01-17 09:32:49 +0200

    Add integration tests

--- src/main/java/com/example/fminder/repositories/ChatRepository.java
+++ src/main/java/com/example/fminder/repositories/ChatRepository.java
@@ -13,6 +13,13 @@ public interface ChatRepository extends JpaRepository<Message, Long> {
             "OR (m.senderId = :receiverId AND m.receiverId = :currentUserId)")
     List<Message> getMessagesBetweenUsers(int currentUserId, int receiverId);
 
-    @Query("SELECT c FROM Message c JOIN (SELECT CASE WHEN c.senderId = :currentUserId THEN c.receiverId WHEN c.receiverId = :currentUserId THEN c.senderId END AS otherUser, MAX(c.timestamp) AS maxTimestamp FROM Message c  WHERE c.receiverId = :currentUserId OR c.senderId = :currentUserId GROUP BY otherUser) lastMsg ON ((c.senderId = :currentUserId AND c.receiverId = lastMsg.otherUser) OR (c.receiverId = :currentUserId AND c.senderId = lastMsg.otherUser)) AND c.timestamp = lastMsg.maxTimestamp")
+    @Query("SELECT m " +
+            "FROM Message m " +
+            "WHERE (m.senderId = :currentUserId OR m.receiverId = :currentUserId) " +
+            "    AND m.timestamp = (SELECT MAX(m2.timestamp) " +
+            "                      FROM Message m2 " +
+            "                      WHERE (m2.senderId = m.senderId AND m2.receiverId = m.receiverId) " +
+            "                         OR (m2.senderId = m.receiverId AND m2.receiverId = m.senderId))")
     List<Message> getLastMessagesWithEachUser(int currentUserId);
+
 }
